0b11531ea1ae81e90422fb736e3b969aa1fe5e63
patriceTchuente <tchuentekammognepatrice@gmail.com>
2022-02-20 11:14:18 +0100

    beaucoup en retard

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # DartDevWeb
 application de dart dans le developpement web
+    pour demarer un projet web dart, utiliser la commande
+        $ dart pub global activate webdev
+        $ dart create -t web-simple monprojet 
+            "monprojet" est le nom que vous donnez au dossier du projet
+        $ cd monprojet"
+        $ webdev serve
+
+TP1: text_editor
+
--- text_editor/web/main.dart
+++ text_editor/web/main.dart
@@ -1,24 +1,34 @@
 import 'dart:html';
+import 'dart:js';
 import 'dart:convert' show json;
 
 TextAreaElement? theEditor;
 
 void main() {
+  // modification du titre de notreediteur
+  Element? apptitle = querySelector("#toolbar");
+  apptitle!.text = "TextEditor";
+
+  // ajout d'un boutton permettant de vider notre editteur
+  ButtonInputElement btnClear = querySelector("#btnClearText") as ButtonInputElement;
+  btnClear.onClick.listen(clearEditor);
+
   theEditor = querySelector("#editor") as TextAreaElement?;
   theEditor!
     ..onKeyUp.listen(handleKeyPress)
     ..text = loadDocument();
 
-  DivElement aptitle = (querySelector("#toolbar")?.text = "TextEditor") as DivElement;
-
-  ButtonInputElement btnClear = querySelector("#btnClearText") as ButtonInputElement;
-  btnClear.onClick.listen(clearEditor);
 }
 
 void clearEditor(MouseEvent event) {
-  theEditor!.text = "";
-  saveDocument();
+  var result = context.callMethod('confirm',
+      ['Are you sure you want to clear the text?']);
+  if (result == true) {
+    theEditor!.text = "";
+    saveDocument();
+  }
 }
+
 void handleKeyPress(KeyboardEvent event) {
   saveDocument();
 }
